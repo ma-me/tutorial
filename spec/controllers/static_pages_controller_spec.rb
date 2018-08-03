692f83120a372f36d9c7147c79748f4e6802cd58
mamem <aemm.ecb@gmail.com>
2018-08-03 16:32:11 +0900

    静的ページチュートリアル 完了

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+
+  describe "GET #home" do
+    it 'should get home' do
+      get :home
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #help' do
+    it 'should get help' do
+      get :help
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #about' do
+    it 'should get about' do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
