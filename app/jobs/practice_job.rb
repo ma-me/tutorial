9ef4c44281ada97edeeab297196926ac78ea0b68
mamem <aemm.ecb@gmail.com>
2019-07-10 22:48:05 +0900

    little fix

--- app/jobs/practice_job.rb
+++ app/jobs/practice_job.rb
@@ -2,7 +2,8 @@ class PracticeJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    puts 'yeah! gonna work background job!!'
+    puts 'yeah! gonna work background job!! :)'
+    sleep 10
     user = User.last
     user.update_attribute(:name, SecureRandom.alphanumeric(10))
   end
